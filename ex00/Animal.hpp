b66961ab99f9f8e8750e3ea1cfc2991f305d5b3d
neoo-n <dana.vauthey@gmail.com>
2025-05-15 12:01:29 +0200

    cta and dog

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   Animal.hpp                                         :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 14:12:45 by dvauthey          #+#    #+#             */
-/*   Updated: 2025/05/14 15:04:54 by dvauthey         ###   ########.fr       */
+/*   Updated: 2025/05/15 11:58:22 by dvauthey         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #ifndef ANIMAL_HPP
 # define ANIMAL_HPP
@@ -21,13 +21,13 @@ class Animal
 		std::string	type;
 
     public:
-        Animal(void);
+        Animal();
         Animal(const Animal& cpy);
         Animal(const std::string type);
-        ~Animal(void);
+        ~Animal();
         Animal &operator=(const Animal& rhs);
 
-		virtual void	makeSound(void) = 0;
+		virtual void 	makeSound() = 0;
 };
 
 #endif
